298095986ff760b0ee6659ad6dbdb7f16468f8a4
shg25 <shg@abeam.ocn.ne.jp>
2022-09-21 02:30:26 +0900

    Jetpack Compose の状態

---
+++ app/src/main/java/com/shg25/aitsu/codelabs/state/ui/WellnessScreen.kt
@@ -0,0 +1,60 @@
+package com.shg25.aitsu.codelabs.state.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.shg25.aitsu.ui.theme.AitsuTheme
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
+    }
+}
+
+
+@Composable
+fun StatefulCounter() {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ })
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewWaterCounter() {
+    AitsuTheme {
+        StatefulCounter()
+    }
+}
+
